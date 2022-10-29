a441ee2458e0c86ade07718253a3b4124633a2fe
Kevin <kevin.cheng.xie@gmail.com>
2022-10-29 15:05:45 -0400

    Added toy problem

--- toy_problem.py
+++ toy_problem.py
@@ -1,30 +1,115 @@
+from typing import Tuple
+import jax
 import jax.numpy as jnp
 from jax import vmap
 from functools import partial
 
-def get_toy_problem_functions():
-    def sample_problem_phi():
-        """
-        Returns:
-            phi: 
-        """
-        return phi
+def get_toy_problem_functions(nwalls=2):
+    # Represents ndim walls, each with 2 holes, q is ndim long and each dim 
+    # corresponds to a wall
+    # This is [nwalls, nholes_per_wall]
+    q_holes = jnp.array([[-1.,1.]]*nwalls)
+    nholes_per_wall = 2
 
-    @partial(jnp.vectorize, signature='(k)->()')
-    def c(q, phi):
-        """ Batched cost function computation """
-        # assert x.ndim == 2
-        # assert x.shape[1] == 2
-        A = jnp.array([[1.,0.2],[-0.3,1.]])
-        # return jnp.einsum('bi,bi->b', x, jnp.einsum('ij,bj->bi',A, x))
-        return jnp.vdot(x,jnp.matmul(A,x))
-    
-    def mock_solution(phi):
+    def get_problem_phi(params):
+        """
+        This is the observable context of the problem 
+        """
+        # Just expose the shift
+        return params[0]
+
+    def sample_problem_params(key, batchsize) -> Tuple:
+        """
+        Args:
+            key: jax RNG key
+        Returns:
+            (phi): Tuple of params
+        """
+        # Small shift in location per wall
+        phi_shift = jax.random.uniform(key, shape=(batchsize,nwalls,), minval=-0.2, maxval=0.2)
+
+        # Weigh each hole differently
+        phi_weight = jax.random.uniform(key, shape=(batchsize,nwalls,nholes_per_wall), minval=0.1, maxval=1.0)
+        return (phi_shift, phi_weight)
+
+    @partial(jnp.vectorize, signature='(),()->()')
+    def gaussian_cost_1d(x, center):
+        return -jnp.exp(-((x-center)*2.)**2)
+
+    def cost(q, prob_params):
+        """ 
+        Cost function computation 
+        Args:
+            q: (ndim) array
+            prob_params: Tuple of arrays representing problem parameters
+        """
+        phi_shift, phi_weight = prob_params
+        # for a single q, calculate its cost to all holes
+
+        # Add dummy "holes" broadcast dimension to q
+        q = jnp.expand_dims(q,-1)
+        cost = gaussian_cost_1d(q, (q_holes+phi_shift))
+        # multiply each hole by weight
+        cost = cost * phi_weight
+        print(cost.shape)
+
+        # Sum over all holes on each wall
+        return jnp.sum(cost, (-2,-1))
+
+    # Batch over problem params
+    @partial(vmap, in_axes=0, out_axes=0) 
+    def mock_solution(prob_params):
         """
         Pretends to do VOO
         Return:
             q_star: Approximate solution to phi    
         """
-        return
+        # Get the hole for each wall with the highest weight
+        phi_shift, phi_weight = prob_params
+        best_hole = jnp.argmax(phi_weight, axis=-1)
+        # For each wall, grab the best hole
+        q_star = (q_holes + phi_shift)[jnp.arange(nwalls),best_hole]
+        return q_star
 
-    return sample_problem_phi, c, mock_solution
+    return sample_problem_params, get_problem_phi, cost, mock_solution
+
+def main():
+    import matplotlib.pyplot as plt
+    nwalls = 2
+    samp_prob, get_phi, cost, mock_sol = \
+        get_toy_problem_functions(nwalls=nwalls)
+
+    key = jax.random.PRNGKey(2)
+    probp = samp_prob(key, batchsize=3)
+
+    # Plot total cost for each wall, with the other wall fixed to 0
+    fig, axs = plt.subplots(1,nwalls, sharey=True,figsize=(4*nwalls,4))
+
+    q_star = mock_sol(probp)[0]
+    print("q_star", q_star.shape)
+    for walli in range(2):
+        ax = axs[walli]
+        ax.set_title(f"wall {walli}")
+
+        if walli==0:
+            qwall0 = jnp.linspace(-3., 3., 32)
+            qwall1 = jnp.zeros_like(qwall0)
+        else:
+            qwall0 = jnp.zeros_like(qwall0)
+            qwall1 = jnp.linspace(-3., 3., 32)
+        qs = jnp.stack([qwall0, qwall1], axis=-1)
+        c = vmap(cost, in_axes=(None,0),out_axes=(0))(qs, probp)
+        cviz = c[0]
+        print(c.shape)
+
+        if walli==0:
+            ax.plot(qwall0, cviz)
+        else:
+            ax.plot(qwall1, cviz)
+        ax.axvline(q_star[walli],color='r')
+
+    fig.savefig("./viz_toy_cost.png")
+
+if __name__ == "__main__":
+    #visualize the problem
+    main()
